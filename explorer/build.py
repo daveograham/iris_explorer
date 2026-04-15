fe465a4b728ae89557d7b19490fe073e232bb687
daveograham <dg130t@Thereus.localdomain>
2026-04-15 15:36:45 +0100

    add sit and stare functionality

--- explorer/build.py
+++ explorer/build.py
@@ -3,7 +3,6 @@ import warnings
 import IPython.display as display
 from ipywidgets import widgets
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 
 from matplotlib.backend_bases import MouseEvent
 
@@ -15,13 +14,14 @@ from enum import Enum
 
 from scipy.optimize import curve_fit
 
-# from .fit_tools import ngen_gauss
-# from .fit_tools import gaussian_bounds
+from .fit_tools import ngen_gauss
+from .fit_tools import gaussian_bounds
+# version to work with a sit and stare
 
 class UI:
     '''
     Class for selecting rasters and centroids for fitting. Setup mostly borrowed from Chris Osborne's Sunspot Selector
-    and heavily inspired by the SSW routine iris_raster_browser
+    and inspired by the SSW routine iris_raster_browser
     
     Parameters
     ----------
@@ -38,10 +38,11 @@ class UI:
         HOLD = "hold"
 
     
-    def __init__(self, filedir, iraster = 0, xpad = 1, gui_scale = 8):
+    def __init__(self, filedir, iraster = 0, xpad = 1, gui_scale = 8, memsave = False, sitandstare = False):
         plt.close('all')
         self.filedir = filedir
-        
+        self.memsave = memsave
+        self.sitandstare = sitandstare
         self.filelist = os.listdir(self.filedir)
         
         #some setup
@@ -58,19 +59,32 @@ class UI:
         #default states
         self.mode = self.Mode.PIXEL
         self.wavslice = -1
-        self.intset = -1
+        self.intset = [0, 50] #default intensity value in % of max (0-100%)
+        self.timeset = [0, 1] #default fraction of time range to use for SNS x axis (odd behaviour when not set to 0 1)
 
-        #fitter
+        #fitter default width
         self.pwid = 0.1
 
-        raster_obj = read_files(startpath, memmap=False)
+        if memsave is True:
+            raster_obj = read_files(startpath, memmap=True)
+        else:
+            raster_obj = read_files(startpath, memmap=False)
+
         keys = raster_obj.keys()
         self.keylist = list(keys)
         self.rasterkey = self.keylist[0]
         
         #default cursor position
-        shape = raster_obj[self.keylist[0]][0].data.shape
-        self.xy = (shape[0] // 2, shape[1] // 2)
+        idims = raster_obj[self.keylist[0]][0].data.shape
+        self.idims = idims
+        self.xy = (idims[0] // 2, idims[1] // 2)
+
+        #default time range
+        if sitandstare == True:
+            self.timelimits = [0, int(idims[0]*self.timeset[1]-1)]
+        else:
+            self.timelimits = [0, int(idims[0]*self.timeset[1]-1)]
+
 
         self.fig, self.ax = plt.subplot_mosaic('''AB
                                                AB
@@ -82,11 +96,16 @@ class UI:
         self.state = {'A':self.rasterkey,
                       'B':self.keylist[-1]}
 
+        self.load_raster(iraster)
         self.setup_buttons(iraster)
 
-        self.load_raster(iraster)
-        self.plot_raster_window(self.rasterkey,'A')
-        self.plot_raster_window(self.keylist[-1],'B')
+        if sitandstare == False:
+            self.plot_raster_window(self.rasterkey,'A')
+            self.plot_raster_window(self.keylist[-1],'B')
+        if sitandstare == True:
+            self.plot_raster_window(self.rasterkey,'A', timelimits=self.timelimits, intset=self.intset, drag=0)
+            self.plot_raster_window(self.keylist[-1],'B', timelimits=self.timelimits, intset=self.intset, drag=0)
+
         self.plot_spectrum(self.rasterkey, self.xy)
 
 
@@ -96,19 +115,25 @@ class UI:
         self.receiver = self.fig.canvas.mpl_connect('button_release_event', self.on_release)
 
         #RASTER SLIDER
-        self.slider = widgets.IntSlider(startingSlider, 0, len(self.filelist)-1, description='File Number')
-        display.display(self.slider)
-        self.slider.observe(self.change_raster, names='value')
+        if self.sitandstare == False:
+            self.expslider = widgets.IntSlider(startingSlider, 0, len(self.filelist)-1, description='File Number')
+            display.display(self.expslider)
+            self.expslider.observe(self.change_raster, names='value')
+        else:
+            self.expslider = widgets.IntSlider(startingSlider, 0, self.idims[0]-1, description='Exposure')
+            display.display(self.expslider)
+            self.expslider.observe(self.change_exposure, names='value')
 
         #WINDOW SELECTION
         self.window_picker = widgets.Dropdown(options=self.keylist, value=self.rasterkey, description='Window:', disabled=False)
         display.display(self.window_picker)
         self.window_picker.observe(self.change_window, names='value')
-        
-        self.intslider = widgets.IntRangeSlider(value=[0, 1000],
+
+        #INTENSITY SLIDER        
+        self.intslider = widgets.IntRangeSlider(value=self.intset,
                                 min=0,
-                                max=1000,
-                                step=10,
+                                max=100,
+                                step=1,
                                 description='Intensity Scale:',
                                 disabled=False,
                                 continuous_update=False,
@@ -118,6 +143,22 @@ class UI:
                             )
         display.display(self.intslider)
         self.intslider.observe(self.update_raster, names='value')
+
+        if self.sitandstare == True:
+            #SNS CLIP SLIDER
+            self.timeslider = widgets.IntRangeSlider(value=[0, int(self.timeset[1]*self.idims[0]-1)],
+                                    min=0,
+                                    max=self.idims[0]*self.timeset[1]-1,
+                                    step=10,
+                                    description='Time Range:',
+                                    disabled=False,
+                                    continuous_update=False,
+                                    orientation='horizontal',
+                                    readout=True,
+                                    readout_format='d',
+                                )
+            display.display(self.timeslider)
+            self.timeslider.observe(self.update_timeslice, names='value')
 
         #START PARAMETER PICKER
         self.paramstartButton = widgets.ToggleButton(
@@ -156,33 +197,46 @@ class UI:
     def load_raster(self, rx):
         '''open raster file into memory'''
         self.filepath = self.filedir + self.filelist[rx]
-        self.raster_obj = read_files(self.filepath, memmap=False)
+        if self.memsave is True:
+            self.raster_obj = read_files(self.filepath, memmap=True)
+        else:
+            self.raster_obj = read_files(self.filepath, memmap=False)
 
-    def plot_raster_window(self, windowkey, subplot, wavpix=None, drag=0, intlimits=-1):
+    def plot_raster_window(self, windowkey, subplot, wavpix=None, drag=0, intset=-1, timelimits=-1):
         raster = self.raster_obj[windowkey][0]
         ldim = raster.data.shape[2]
+        xdim = raster.data.shape[0]
+
+        if timelimits == -1:
+            timelimits = [0,xdim-1]
+
         #clip to sensible range and mean over wavelength
         #DEFAULT START PLOT -
         if wavpix is None:
-            clipped = np.mean(np.clip(raster.data,0,100000),axis=2)
-        
+            clipped = np.mean(np.clip(raster.data[timelimits[0]:timelimits[1],:,:],0,100000),axis=2)
+
+        if intset == -1:
+            intset = [0, 50]
+
         #WHEN CLICKED
         if wavpix is not None:
             if drag == 0:
-                clipped = np.clip(raster[:,:,wavpix].data,0,100000)
+                clipped = np.clip(raster[timelimits[0]:timelimits[1],:,wavpix].data,0,100000)
             else:
                 lowx = np.clip(np.min([self.wavpix_down, self.wavpix_up]), 0, ldim)
-                highx = np.clip(np.max([self.wavpix_down,self.wavpix_up]), 0, ldim)
+                highx = np.clip(np.max([self.wavpix_down, self.wavpix_up]), 0, ldim)
                 self.wavslice = (lowx,highx)
-                clipped = np.mean(np.clip(raster[:,:,lowx:highx].data,0,100000),axis=2)
+                clipped = np.mean(np.clip(raster[timelimits[0]:timelimits[1],:,lowx:highx].data,0,100000),axis=2)
         
-        if intlimits == -1:
-            self.ax[subplot].imshow(clipped.T, origin='lower', aspect=1/self.stretch, interpolation='none')
+        if timelimits != -1:
+            self.ax[subplot].imshow(clipped.T, origin='lower', aspect='auto', interpolation='none', vmin=np.max(clipped)*intset[0]*0.01, vmax=np.max(clipped)*intset[1]*0.01)
         else:
-            self.ax[subplot].imshow(clipped.T, origin='lower', aspect=1/self.stretch, interpolation='none', vmin=intlimits[0], vmax=intlimits[1])
+            self.ax[subplot].imshow(clipped.T, origin='lower', aspect='auto', interpolation='none', vmin=np.max(clipped)*intset[0]*0.01, vmax=np.max(clipped)*intset[1]*0.01)
+        
         self.ax[subplot].set_title(windowkey)
 
         self.state[subplot] = windowkey
+        self.timelimits = timelimits
 
     def get_wave(self, windowkey):
         #ignore annoying astropy warnings
@@ -190,22 +244,24 @@ class UI:
             warnings.simplefilter("ignore")
             return self.raster_obj[windowkey][0].axis_world_coords("em.wl")[0].to("AA")
 
-    def plot_spectrum(self, windowkey, dataloc, click=-1):
+    def plot_spectrum(self, windowkey, plotloc, click=-1):
         raster = self.raster_obj[windowkey][0]
         wave = self.get_wave(windowkey)
-        x = dataloc[0]
-        y = dataloc[1]
+
+        xdata = plotloc[0] + self.timelimits[0]
+        ydata = plotloc[1]
+
         self.ax['C'].clear()
         
-        self.ax['C'].set_ylim([0, 1.2*np.max(raster[x,y].data)])
-        self.ax['C'].step(wave, raster[x,y].data)
+        self.ax['C'].set_ylim([0, 1.2*np.max(raster[xdata,ydata].data)])
+        self.ax['C'].step(wave, raster[xdata ,ydata].data)
         
         if self.markerA is not None:
             self.markerA[0].remove()
             self.markerB[0].remove()
         
-        self.markerA = self.ax['A'].plot(x,y, marker='+',color='w')  #markersize
-        self.markerB = self.ax['B'].plot(x,y, marker='+',color='w')
+        self.markerA = self.ax['A'].plot(plotloc[0],plotloc[1], marker='+',color='r', scalex=False, scaley=False, zorder=10)  #markersize
+        self.markerB = self.ax['B'].plot(plotloc[0],plotloc[1], marker='+',color='r', scalex=False, scaley=False, zorder=10)
         
         #plot marker for selection
         if click > 0:
@@ -218,9 +274,13 @@ class UI:
             return
         
         pixloc = (int(event.xdata+0.5), int(event.ydata+0.5))
+        
         #IF A RASTER CLICK ============================        
         if event.inaxes is self.ax['A'] or event.inaxes is self.ax['B']:
             self.xy = pixloc
+            if self.sitandstare == True:
+                self.expslider.value = pixloc[0] - self.timelimits[0]
+
             self.plot_spectrum(self.rasterkey, self.xy)
             if self.mode is self.Mode.HOLD:
                 self.fitter(self.rasterkey, self.xy)
@@ -247,7 +307,6 @@ class UI:
                 #interpolate to get clicked pixel number from wavelength value
                 self.wavpix_down = np.searchsorted(wave.value, self.wavloc_down[0])
                 
-                #self.plot_raster_window(self.rasterkey,'A', self.wavpix_down)
                 self.plot_spectrum(self.rasterkey, self.xy, click=1)
             
             if self.mode == self.Mode.FIT or self.mode == self.Mode.HOLD:
@@ -275,17 +334,17 @@ class UI:
                 self.wavpix_up = np.searchsorted(wave.value, self.wavloc_up[0])
 
                 if self.wavloc_down[0] == event.xdata:
-                    self.plot_raster_window(self.rasterkey,'A', self.wavpix_down)
+                    self.plot_raster_window(self.rasterkey,'A', self.wavpix_down, timelimits=self.timelimits, intset=self.intset)
                     return
 
                 if clickdiff < self.dwave*500.0 and clickdiff > self.dwave:  #check clicks are more than 1 pixel away
                     self.ax['C'].plot((event.xdata,event.xdata),(0,100000),color='g',linestyle='--')
                     #interpolate to get clicked pixel number from wavelength value
-                    self.plot_raster_window(self.rasterkey,'A', self.wavpix_up, drag=1)
+                    self.plot_raster_window(self.rasterkey,'A', self.wavpix_up, timelimits=self.timelimits, intset=self.intset, drag=1)
                     
                     self.shade_spectrum(wave)
                 else:
-                    self.plot_raster_window(self.rasterkey,'A', self.wavpix_down)
+                    self.plot_raster_window(self.rasterkey,'A', self.wavpix_down, timelimits=self.timelimits, intset=self.intset)
 
     def shade_spectrum(self, wave):
         self.ax['C'].fill_between(wave[self.wavpix_down:self.wavpix_up].value, 0, 100000, facecolor='green',alpha=0.25)
@@ -310,7 +369,7 @@ class UI:
         bounds = gaussian_bounds(self.pstart)
 
         yparam, ycov = curve_fit(ngen_gauss, wave, spec, p0 = self.pstart, bounds=bounds)
-        #yparam, ycov = curve_fit(ngen_gauss, wav_range, spec, p0 = self.pstart, bounds=self.bounds)
+
         self.yparam = yparam
         fit = ngen_gauss(wave.value, *yparam)
         self.ax['C'].plot(wave, fit, linestyle='--')
@@ -354,7 +413,7 @@ class UI:
         return
 
     def change_window(self, event):
-        self.plot_raster_window(event['new'],'A')
+        self.plot_raster_window(event['new'],'A', timelimits=self.timelimits)
         self.rasterkey = event['new']
         #clear any wavelength selection here
         self.wavslice = -1
@@ -366,8 +425,8 @@ class UI:
     def change_raster(self, event):
         #go between to pass the event object into my own function
         self.load_raster(event['new'])
-        self.plot_raster_window(self.rasterkey,'A', intlimits=self.intset)
-        self.plot_raster_window('Mg II k 2796','B')
+        self.plot_raster_window(self.rasterkey,'A', intset=self.intset, timelimits=self.timelimits)
+        self.plot_raster_window('Mg II k 2796','B', intset=self.intset, timelimits=self.timelimits)
         #update spectrum and markers on raster
         if hasattr(self, 'xy'):
             self.plot_spectrum(self.rasterkey, self.xy)
@@ -376,15 +435,36 @@ class UI:
         if self.mode is self.Mode.HOLD:
             self.fitter(self.rasterkey, self.xy)
 
+    def change_exposure(self, event):
+        #update plots for new sit and stare exposure selection
+
+        if hasattr(self, 'xy'):
+            #make new data coord for time and y slit pos
+            self.plot_spectrum(self.rasterkey, [event['new'], self.xy[1]])
+        if self.wavslice != -1:   #hasattr(self, 'wavslice'):
+            self.plot_slice()
+        if self.mode is self.Mode.HOLD:
+            self.fitter(self.rasterkey, [event['new'],self.xy[1]])
+
     def update_raster(self, event):
-        #update raster intensity scale
-        #self.intevent = event
+        #update raster intensity scale - takes percentage of maximum as the scale
+        self.intset = event['new']
+
         if self.wavslice != - 1:
-            self.plot_raster_window(self.rasterkey,'A',intlimits=event['new'],drag=1)
+            self.plot_raster_window(self.rasterkey,'A',intset=event['new'],timelimits=self.timelimits, drag=1)
         else:
-            self.plot_raster_window(self.rasterkey,'A',intlimits=event['new'],drag=0)
+            self.plot_raster_window(self.rasterkey,'A',intset=event['new'],timelimits=self.timelimits, drag=0)
 
         self.intset = event['new']
+
+    def update_timeslice(self, event):
+        self.timelimits = event['new']
+        self.plot_raster_window(self.rasterkey,'A', intset=self.intset, timelimits = event['new'], drag=0)
+        self.plot_raster_window(self.keylist[-1],'B', intset=self.intset, timelimits = event['new'], drag=0)
+
+        if hasattr(self, 'xy'):
+            #make new data coord for time and y slit pos
+            self.plot_spectrum(self.rasterkey, [self.xy[0]-self.timelimits[0], self.xy[1]])
 
     def disconnect_matplotlib(self, _):
         self.fig.canvas.mpl_disconnect(self.receiver)
--- explorer/fit_tools.py
+++ explorer/fit_tools.py
@@ -1,33 +1,7 @@
 import numpy as np
 
-### VARIOUS TOOLS FOR SPECTRAL FITTING ###
-def hfit(value, omin, omax, nmin, nmax):
-    '''houdini style fit function'''
-    norm = np.clip((value - omin) / (omax - omin), 0.0, 1.0)
-    return (1-norm)*nmin + norm*nmax
+### TOOLS FOR SPECTRAL FITTING ###
 
-
-def single_gauss(wav, I, wav0, sigma):
-    '''I[0], wav0[1], sigma[2]'''
-    ygauss = I * np.exp(-(wav - wav0)**2.0/(2.0 * (sigma**2.0)))
-    return ygauss
-
-
-def ngen_gauss_old(wav, *P):
-    '''Takes parameters P in form: I[0], wav0[1], sigma[2], I[1]....'''
-
-    if len(P)%3 == 0:
-        nprofiles = int(np.floor(len(P)/3))
-        ygauss = 0.0
-
-        for n in range(nprofiles):
-            ygauss += P[3*n] * np.exp(-(wav - P[3*n+1])**2.0/(2.0 * (P[3*n+2]**2.0)))
-
-        return ygauss
-    else:
-        print('Parameter number mismatch - expects sets of 3 - I/centroid/sigma')
-        return 0
-    
 def ngen_gauss(wav, *P):
     '''Takes a list [I_1, cen_1, sigma_1, I_N, cen_N, sigma_N..] or a numpy array of dims (Ngaussian, 3) with columns I, centroid, sigma'''
     P = np.asarray(P)
@@ -45,81 +19,6 @@ def ngen_gauss(wav, *P):
         gauss_total = gauss_array.sum(axis=0)
         
         return gauss_total
-    
-
-def gaussian_setup(wav, spec, ngauss=1, **kwargs):
-    '''Set default starting parameters and boundaries. Takes: wavelength base, example spectrum,
-      number of gaussians (optional), optional starting parameters params=[I,cen,sigma] '''
-
-    #percentage range for limits
-    deltaI = 0.3
-    deltacen = 0.25
-    deltasig = 0.5
-    #sigma width limits
-    lowsigma = 0.025
-    highsigma = 2.0
-
-    if 'params' in kwargs.keys():
-        P = np.asarray(kwargs['params'])
-        #if P.size % 3.0 != 0:
-        #    print('Parameter number mismatch - expects array or list of I/centroid/sigma per line')
-        #    return -1
-        #else:
-
-        ngauss = int(P.size/3.0)
-        #if a list reshape into an array Ngauss X 3
-        if P.ndim < 2:
-            P = P.reshape((ngauss,3))
-    else:
-        #default starting I
-        imax = np.max(spec)
-        i_start = imax * (1.0 - (0.1 * float(ngauss)))
-        #default starting centroids
-        wx = wav.size
-        if ngauss % 2 == 0:
-            ldelta = int(wx / (ngauss+1))
-            lx = np.arange(0,wx,ldelta)
-            if ngauss == 2:
-                lx = lx[1:]
-            else:
-                lx = lx[1:-1]
-        else:
-            ldelta = int(wx / (ngauss+1))
-            lx = np.arange(0,wx,ldelta)
-            lx = lx[1:-1]
-        cen_start = wav[lx]
-        #default starting width
-        omin = 0
-        omax = 5000.0
-
-        norm = np.clip((i_start - omin) / (omax - omin), 0.0, 1.0)
-        w_start = (1-norm) * lowsigma + norm * highsigma
-
-        P = np.zeros((ngauss,3))
-        #SET STARTING PARAMETERS
-        for i in range(ngauss):
-            P[i,0] = i_start
-            P[i,1] = cen_start[i]
-            P[i,2] = w_start
-        #========================================================
-    
-    lowbound = []
-    highbound = []
-
-    for i in range(ngauss):
-        #I
-        lowbound.append(P[i,0]*(1-deltaI))
-        highbound.append(P[i,0]*(1+deltaI))
-        #cen
-        lowbound.append(P[i,1]-deltacen)
-        highbound.append(P[i,1]+deltacen)
-        #sigma
-        lowbound.append(np.clip(P[i,2]*(1-deltasig), lowsigma, highsigma))
-        highbound.append(np.clip(P[i,2]*(1+deltasig), lowsigma, highsigma))
-
-    return P, tuple((lowbound, highbound))
-
-
 
 def gaussian_bounds(P, ilow=False):
     '''Set default parameter boundaries from a list of starting parameters'''
@@ -159,34 +58,3 @@ def gaussian_bounds(P, ilow=False):
             highbound.append(highsigma)
 
         return tuple((lowbound, highbound))
-
-
-def gaussian_bounds_old(pstart):
-    '''Set default parameter boundaries from a list of parameters'''
-    ngauss = np.floor(int(len(pstart)/3.0))
-
-    deltaI = 0.4
-    deltacen = 0.3
-    lowsigma = 0.025
-    highsigma = 2.0
-
-    lowbound = []
-    highbound = []
-    for i in range(ngauss):
-        #I
-        lowbound.append(pstart[i*3]*(1-deltaI))
-        highbound.append(pstart[i*3]*(1+deltaI))
-        #cen
-        lowbound.append(pstart[i*3+1]-deltacen)
-        highbound.append(pstart[i*3+1]+deltacen)
-        #sigma
-        lowbound.append(lowsigma)
-        highbound.append(highsigma)
-
-    return tuple((lowbound, highbound))
-
-def start_params(spec, ngauss, *P):
-    '''set start parameters. Requires number gaussians. Optional parameters in form I_1.. I_N, C_1.. C_N, W_1.. W_N.
-    Defaults are set for missing int centroid or width'''
-    P1 = np.max(spec)
-    return P1
